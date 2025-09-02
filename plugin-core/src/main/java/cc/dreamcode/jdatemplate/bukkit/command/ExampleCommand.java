472d2b0be24280efd5a9238185015efd8eb4052f
ForeX03 <kofdextv@gmail.com>
2025-09-02 04:08:38 +0200

    config update

--- plugin-core/src/main/java/cc/dreamcode/jdatemplate/bukkit/command/ExampleCommand.java
+++ plugin-core/src/main/java/cc/dreamcode/jdatemplate/bukkit/command/ExampleCommand.java
@@ -10,7 +10,7 @@ import eu.okaeri.configs.exception.OkaeriException;
 import eu.okaeri.injector.annotation.Inject;
 import lombok.RequiredArgsConstructor;
 
-@Command(name = "dreamsmp", aliases = {"dsmp"})
+@Command(name = "jdatemplate")
 @RequiredArgsConstructor(onConstructor_ = @Inject)
 public class ExampleCommand implements CommandBase {
 
@@ -18,7 +18,7 @@ public class ExampleCommand implements CommandBase {
     private final MessageConfig messageConfig;
 
     @Async
-    @Permission("dreamsmp.reload")
+    @Permission("jdatemplate.reload")
     @Executor(path = "reload", description = "Przeladowuje konfiguracje.")
     BukkitNotice reload() {
         final long time = System.currentTimeMillis();
@@ -36,73 +36,4 @@ public class ExampleCommand implements CommandBase {
                     .with("error", e.getMessage());
         }
     }
-
-    @Async
-    @Permission("dreamsmp.nether")
-    @Completion(arg = "state", value = { "on", "off" })
-    @Executor(path = "nether", description = "Włącza/wyłącza nether")
-    BukkitNotice toggleNether(@Arg String state) {
-        if (state.equalsIgnoreCase("on")) {
-            if (this.pluginConfig.portalConfig.netherEnabled) {
-                return this.messageConfig.netherAlreadyEnabled;
-            }
-
-            this.pluginConfig.portalConfig.netherEnabled = true;
-            this.pluginConfig.save();
-
-            this.messageConfig.netherEnabledBroadcast.sendAll();
-
-            return this.messageConfig.netherEnabled;
-        }
-
-        if (state.equalsIgnoreCase("off")) {
-            if (!this.pluginConfig.portalConfig.netherEnabled) {
-                return this.messageConfig.netherAlreadyDisabled;
-            }
-
-            this.pluginConfig.portalConfig.netherEnabled = false;
-            this.pluginConfig.save();
-
-            this.messageConfig.netherDisabledBroadcast.sendAll();
-
-            return this.messageConfig.netherDisabled;
-        }
-
-        return this.messageConfig.usage.with("label", "/dsmp nether on/off");
-    }
-
-    @Async
-    @Permission("dreamsmp.end")
-    @Completion(arg = "state", value = { "on", "off" })
-    @Executor(path = "end", description = "Włącza/wyłącza nether")
-    BukkitNotice toggleEnd(@Arg String state) {
-        if (state.equalsIgnoreCase("on")) {
-            if (this.pluginConfig.portalConfig.endEnabled) {
-                return this.messageConfig.endAlreadyEnabled;
-            }
-
-            this.pluginConfig.portalConfig.endEnabled = true;
-            this.pluginConfig.save();
-
-            this.messageConfig.endEnabledBroadcast.sendAll();
-
-            return this.messageConfig.endEnabled;
-        }
-
-        if (state.equalsIgnoreCase("off")) {
-            if (!this.pluginConfig.portalConfig.endEnabled) {
-                return this.messageConfig.endAlreadyDisabled;
-            }
-
-            this.pluginConfig.portalConfig.endEnabled = false;
-            this.pluginConfig.save();
-
-            this.messageConfig.endDisabledBroadcast.sendAll();
-
-            return this.messageConfig.endDisabled;
-        }
-
-        return this.messageConfig.usage.with("label", "/dsmp end on/off");
-    }
-
 }
